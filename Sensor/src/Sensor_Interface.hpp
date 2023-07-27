07986ca293130e6a85a685be76066d1dfc36db48
grasmanek94 <github@gz0.nl>
2023-07-27 23:41:05 +0200

    Prepare for SENSOR_LOCATION data

--- Sensor/src/Sensor_Interface.hpp
+++ Sensor/src/Sensor_Interface.hpp
@@ -1,5 +1,17 @@
 #pragma once
 
+enum SENSOR_LOCATION {
+	SENSOR_LOCATION_UNKNOWN,
+	SENSOR_LOCATION_FLOOR_0_LIVING_ROOM,
+	SENSOR_LOCATION_FLOOR_1_BATHROOM,
+	SENSOR_LOCATION_FLOOR_1_BEDROOM_FRONT,
+	SENSOR_LOCATION_FLOOR_1_BEDROOM_LEFT,
+	SENSOR_LOCATION_FLOOR_1_BEDROOM_RIGHT,
+	SENSOR_LOCATION_FLOOR_2_FRESH_AIR_INLET,
+	SENSOR_LOCATION_FLOOR_2_USED_AIR_OUTLET,
+	SENSOR_LOCATION_OUTSIDE
+};
+
 class Sensor_Interface {
 public:
 	Sensor_Interface() {}
@@ -19,4 +31,5 @@ public:
 	virtual float get_relative_humidity() { return 0.0f; }
 	virtual int get_co2_ppm() { return 0; }
 	virtual int get_meter_status() { return 0; }
+	virtual SENSOR_LOCATION get_location() { return SENSOR_LOCATION_UNKNOWN; }
 };
